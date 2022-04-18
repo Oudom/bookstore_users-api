7d5912d4aa1c546d10484b4079767daf624a2def
Oudom Ung <oudom.ung@emuhsd.org>
2022-04-17 20:26:01 -0700

    Add controllers and domain

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"log"
-)
+import "github.com/Oudom/bookstore_users-api/app"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	err := r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
+	app.StartApplication()
 }
