0861dc8a4da2e123bd4dcd3c98e0f8bec31cb6da
Oudom Ung <oudom.ung@emuhsd.org>
2022-04-18 17:31:38 -0700

    Add error handling...

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,11 +3,40 @@ package users
 import (
 	"net/http"
 
+	"github.com/Oudom/bookstore_users-api/domain/users"
+	"github.com/Oudom/bookstore_users-api/services"
+	"github.com/Oudom/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.RestErr{
+			Message: "invalid json body",
+			Status:  http.StatusBadRequest,
+			Error:   "bad_request",
+		}
+		c.JSON(restErr.Status, restErr)
+	}
+	// fmt.Println(user)
+	// bytes, err := ioutil.ReadAll(c.Request.Body)
+	// if err != nil {
+	// 	//TODO: Handle error
+	// 	return
+	// }
+	// if err := json.Unmarshal(bytes, &user); err != nil {
+	// 	fmt.Println(err.Error())
+	// 	//TODO: Handle json error
+	// 	return
+	// }
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//TODO: Handle user creation error
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
