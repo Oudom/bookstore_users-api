715e0591fd93ee9fd4413ce6c5c715d9e667b93c
ladmin <bad8mjun>
2022-04-26 17:13:25 -0700

    Add DAO access

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -9,27 +9,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// fmt.Println(user)
+// bytes, err := ioutil.ReadAll(c.Request.Body)
+// if err != nil {
+// 	//TODO: Handle error
+// 	return
+// }
+// if err := json.Unmarshal(bytes, &user); err != nil {
+// 	fmt.Println(err.Error())
+// 	//TODO: Handle json error
+// 	return
+// }
 func CreateUser(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		restErr := errors.RestErr{
-			Message: "invalid json body",
-			Status:  http.StatusBadRequest,
-			Error:   "bad_request",
-		}
+		restErr := errors.NewBadRequestError("invalid json body")
 		c.JSON(restErr.Status, restErr)
+		return
 	}
-	// fmt.Println(user)
-	// bytes, err := ioutil.ReadAll(c.Request.Body)
-	// if err != nil {
-	// 	//TODO: Handle error
-	// 	return
-	// }
-	// if err := json.Unmarshal(bytes, &user); err != nil {
-	// 	fmt.Println(err.Error())
-	// 	//TODO: Handle json error
-	// 	return
-	// }
 
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
---
+++ domain/users/user_dao.go
@@ -0,0 +1,30 @@
+package users
+
+import (
+	"fmt"
+
+	"github.com/Oudom/bookstore_users-api/utils/errors"
+)
+
+var (
+	usersDB = make(map[int64]*User)
+)
+
+func (user *User) Get() *errors.RestErr {
+	result := usersDB[user.Id]
+	if result == nil {
+		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	}
+
+	user.Id = result.Id
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+
+	return nil
+}
+
+func Save(user *User) *errors.RestErr {
+	return nil
+}
