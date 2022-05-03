cff6dbe2a4e2bc891a21a077a4dddbe202a4b065
ladmin <bad8mjun>
2022-05-03 13:03:10 -0700

    OS variables

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -4,20 +4,33 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	mysql_users_username = "mysql_users_username"
+	mysql_users_password = "mysql_users_password"
+	mysql_users_host     = "mysql_users_host"
+	mysql_users_schema   = "mysql_users_schema"
+)
+
 var (
 	Client *sql.DB
+
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_password)
+	host     = os.Getenv(mysql_users_host)
+	schema   = os.Getenv(mysql_users_schema)
 )
 
 func init() {
 	datasourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		"root",
-		"4449345",
-		"127.0.0.1:3306",
-		"users_db")
+		username,
+		password,
+		host,
+		schema)
 
 	var err error
 	Client, err = sql.Open("mysql", datasourceName)
