29bf674273fdc84e3adfdf1611431ad06a3139ca
ladmin <bad8mjun>
2022-05-23 16:49:30 -0700

    Add DELETE user endpoint

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -10,7 +10,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CreateUser(c *gin.Context) {
+func getUserId(userIdParam string) (int64, *errors.RestErr) {
+	userId, userErr := strconv.ParseInt(userIdParam, 10, 64)
+	if userErr != nil {
+		return 0, errors.NewBadRequestError("user id should be a number")
+	}
+	return userId, nil
+}
+func Create(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		restErr := errors.NewBadRequestError("invalid json body")
@@ -26,11 +33,10 @@ func CreateUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, result)
 }
 
-func GetUser(c *gin.Context) {
-	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
-	if userErr != nil {
-		err := errors.NewBadRequestError("user Id should be a number")
-		c.JSON(err.Status, err)
+func Get(c *gin.Context) {
+	userId, idErr := getUserId(c.Param("user_id"))
+	if idErr != nil {
+		c.JSON(idErr.Status, idErr)
 		return
 	}
 	user, getErr := services.GetUser(userId)
@@ -41,11 +47,11 @@ func GetUser(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
-func UpdateUer(c *gin.Context) {
-	UserId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
-	if userErr != nil {
-		err := errors.NewBadRequestError("user id should be a number")
-		c.JSON(err.Status, err)
+func Update(c *gin.Context) {
+	userId, idErr := getUserId(c.Param("user_id"))
+	if idErr != nil {
+		c.JSON(idErr.Status, idErr)
+		return
 	}
 	var user users.User
 
@@ -55,7 +61,7 @@ func UpdateUer(c *gin.Context) {
 		return
 	}
 
-	user.Id = UserId
+	user.Id = userId
 	isPartial := c.Request.Method == http.MethodPatch
 	result, err := services.UpdateUser(isPartial, user)
 	if err != nil {
@@ -65,6 +71,14 @@ func UpdateUer(c *gin.Context) {
 	c.JSON(http.StatusOK, result)
 }
 
-func SearchUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+func Delete(c *gin.Context) {
+	userId, idErr := getUserId(c.Param("user_id"))
+	if idErr != nil {
+		c.JSON(idErr.Status, idErr)
+		return
+	}
+	if err := services.DeleteUser(userId); err != nil {
+		c.JSON(err.Status, err)
+	}
+	c.JSON(http.StatusOK, map[string]string{"status": "deleted"})
 }
